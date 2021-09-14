113c47fcc491b64fd3b830a80dd735fc53aba88d
MoXiaoxiao <1228000105@qq.com>
2021-09-14 14:18:30 +0800

    添加FileReaderUtils类的readFileToString方法

--- src/test/java/org/moxiaoxiao/AppTest.java
+++ src/test/java/org/moxiaoxiao/AppTest.java
@@ -2,19 +2,22 @@ package org.moxiaoxiao;
 
 import static org.junit.Assert.assertTrue;
 
+import com.moxiaoxiao.FileReaderUtils;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testReadFileToString() throws IOException {
+        String path = "log.log";
+        String context= FileReaderUtils.readFileToString(path);
+        System.out.println(context);
     }
 }
